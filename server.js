a38322e5ca43556a99144ecaa51c4dc70a945775
Miguel Valencia <miguel.s.valencia@gmail.com>
2014-08-22 20:55:06 -0700

    Added a couple routes. This is super easygit commit -m ! The Mongoose ORM rules! I hope Sequalizer is just as awesome.

--- server.js
+++ server.js
@@ -7,11 +7,17 @@
 var express    = require('express'); 		// call express
 var app        = express(); 				// define our app using express
 var bodyParser = require('body-parser');
+var Bear       = require('./app/models/bear');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+var mongoose   = require('mongoose');
+//mongoose.connect('mongodb://127.0.0.1/:27017'); // connect to our database
+mongoose.connect('127.0.0.1:27017/beardb'); // connect to our database
+
 
 var port = process.env.PORT || 8080; 		// set our port
 
@@ -19,18 +25,78 @@ var port = process.env.PORT || 8080; 		// set our port
 // =============================================================================
 var router = express.Router(); 				// get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+//    do logging
+    console.log('Something is happening.');
+    next();
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });	
-	});
+});
 
-	// more routes for our API will happen here
+// more routes for our API will happen here
 
-	// REGISTER OUR ROUTES -------------------------------
-	// all of our routes will be prefixed with /api
-	app.use('/api', router);
+// on routes that end in /bears
+router.route('/bears')
+//    create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res){
+        var bear = new Bear(); // create new instance of bear model
+        bear.name = req.body.name; // set the bear's name (comes from the request)
 
-	// START THE SERVER
-	// =============================================================================
-	app.listen(port);
-	console.log('Magic happens on port ' + port);
+//        save the bear and check for errors
+        bear.save(function(err){
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear created!' });
+        });
+    })
+
+    .get(function(req, res){
+        Bear.find(function(err, bears){
+            if (err)
+                res.send(err);
+
+            res.json(bears);
+        });
+    });
+
+router.route('/bears/:bear_id')
+
+    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res){
+        Bear.findById(req.params.bear_id, function(err, bear){
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
+    })
+
+    // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+    .put(function(req, res){
+        Bear.findById(req.params.bear_id, function(err, bear){
+            if (err)
+                res.send(err)
+
+            bear.name = req.body.name;
+
+            bear.save(function(err){
+                if (err)
+                    res.send(err)
+
+                res.json({ message: "Bear updated!" });
+            });
+        });
+    });
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
