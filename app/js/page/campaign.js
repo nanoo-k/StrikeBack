db196c7af48635feb31f96e405468a17dfdb4c85
Miguel Valencia <miguel.s.valencia@gmail.com>
2014-09-10 12:26:57 -0700

    User can 'sign' up to campaign, which creates user, but doesn't yet associate user with campaign.

--- app/js/page/campaign.js
+++ app/js/page/campaign.js
@@ -5,9 +5,10 @@ define([
     'underscore',
     'layout/global',
     'model/campaign',
+    'model/user',
     'text!/js/template/campaign.ejs',
     'marionette',
-], function ( $, _, layout, Campaign, Template) {
+], function ( $, _, layout, Campaign, User, Template) {
 
     var TestView = Backbone.Marionette.ItemView.extend({
         template: _.template(Template),
@@ -17,6 +18,7 @@ define([
             this.model = new Campaign();
             this.model.fetch({
                 success: $.proxy(function(){
+                    // I'm setting them after the fact like this and not in the templates bcuz this page initializes and renders without a model set, so there's no template!
                     this.setAttrs();
                 }, this)
             });
@@ -27,6 +29,26 @@ define([
             this.$el.find('#name').html(this.model.get('name'));
             this.$el.find('#target').html(this.model.get('target'));
             this.$el.find('#callToAction').html(this.model.get('callToAction'));
+        },
+
+        events: {
+            "click #join" : "onUserJoin"
+        },
+
+        onUserJoin: function(){
+            var user = new User();
+            user.set({
+                'username' : this.$el.find('#email').val(),
+                'email': this.$el.find('#email').val(),
+                'phone': this.$el.find('#phone').val(),
+                'password': this.$el.find('#password').val()
+            });
+
+            user.save({
+                success: function(model){
+                    console.log(model);
+                }
+            });
         }
 
 
