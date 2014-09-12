bd2bea43886de98ae309da40d8eb28a6f510588b
Miguel Valencia <miguel.s.valencia@gmail.com>
2014-09-12 15:13:35 -0700

    Deleted an unused DB schema model.

--- models/CampaignUsers.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var CampaignUsers = sequelize.define('CampaignUsers', {
-    campaignId: DataTypes.BIGINT,
-    userId: DataTypes.BIGINT,
-  })
- 
-  return CampaignUsers
-}
