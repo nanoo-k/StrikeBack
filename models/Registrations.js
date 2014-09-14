9fef1c7cac731f65f4bc653bfa431e1f21e67644
Miguel Valencia <miguel.s.valencia@gmail.com>
2014-09-13 21:19:11 -0700

    Deleted registrations.js

--- models/Registrations.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Registrations = sequelize.define('Registrations', {
-    campaignId: DataTypes.BIGINT,
-    userId: DataTypes.BIGINT,
-  })
- 
-  return Registrations
-}
