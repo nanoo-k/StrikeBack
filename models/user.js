c68c31e30b121edd071b30d1da1c22631db5b77b
Miguel Valencia <miguel.s.valencia@gmail.com>
2014-09-16 12:34:59 -0700

    Added instanceMethods to User model to encrypt and check password.

--- models/user.js
+++ models/user.js
@@ -1,9 +1,28 @@
+var bcrypt = require('bcrypt');
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     username: DataTypes.STRING,
     password: DataTypes.STRING,
-    phone: { type: DataTypes.BIGINT, allowNull: true },
-    email: { type: DataTypes.STRING, allowNull: true }
+    phone: { type: DataTypes.BIGINT, allowNull: true, unique: true },
+    email: { type: DataTypes.STRING, allowNull: true, unique: true }
+  },{
+  	instanceMethods: {
+      setPassword: function(password, done) {
+        return bcrypt.genSalt(10, function(err, salt) {
+          return bcrypt.hash(password, salt, function(error, encrypted) {
+            this.password = encrypted;
+            this.salt = salt;
+            return done();
+          });
+        });
+      },
+      verifyPassword: function(password, done) {
+        return bcrypt.compare(password, this.password, function(err, res) {
+          return done(err, res);
+        });
+      }
+    }
   })
  
   return User
