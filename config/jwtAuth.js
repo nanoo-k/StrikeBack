d124f2ecf8eaffaa76da39244ed871baabb1f313
Miguel Valencia <miguel.s.valencia@gmail.com>
2014-09-20 14:17:32 -0700

    Set token in JSON response rather than send it from the middleware.

--- config/jwtAuth.js
+++ config/jwtAuth.js
@@ -8,9 +8,12 @@ var   url 			= require('url')
 	, User 			= require('../models/user')
 	, jwt 			= require('jwt-simple')
 	, secret 		= require('./secret.js')
-	, db            = require('../models');
+	, db            = require('../models')
+	, passport      = require('passport')
 
 module.exports = function(req, res, next){
+
+	// Check whether token exists ====================================================
 	// Parse the URL, we might need this
 	var parsed_url = url.parse(req.url, true)
 	/**
@@ -38,6 +41,39 @@ module.exports = function(req, res, next){
 			return next()
 		}
 	} else {
-		next()
+
+		// If token doesn't exist, check whether user creds were sent =====================
+		// Place token, expires and user in res.json if creds pass
+
+		passport.authenticate('local-login', function(err, user, info) {
+          if (err) {
+            return next(err); // will generate a 500 error
+          }
+          
+          if (! user) {
+            return res.status(401).send({ success : false, message : info.message });
+          }
+            
+          var expires = moment().add('days', 7).valueOf();
+          var token = jwt.encode({
+            iss: user.id,
+            exp: expires
+          }, app.get('jwtTokenSecret'));
+           
+          // This sends the token, expires and user info
+          // Is identical to res.send() except that it converts non-JSON objs to JSON
+          // Need to convert this so that it just passes an obj to be sent back to the user, but not sent yet.
+          // Maybe use res.set() http://expressjs.com/api.html#res.send
+          req.token({
+            token : token,
+            expires: expires,
+            user: user.toJSON()
+          });
+
+          return next()
+
+        })(req, res, next)
+
+        // next()
 	}
 }
