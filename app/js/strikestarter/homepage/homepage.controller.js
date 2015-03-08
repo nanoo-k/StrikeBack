e364ece8a70018a1f8c49638b31d657d047e6162
Miguel Valencia <miguel.s.valencia@gmail.com>
2015-03-08 00:40:11 -0800

    Save incoming data to glb obj. Test whether it works.

--- app/js/strikestarter/homepage/homepage.controller.js
+++ app/js/strikestarter/homepage/homepage.controller.js
@@ -6,10 +6,10 @@
 
 strikestarter.controller('Homepage', [ '$scope', '$sce', 'Campaigns', function($scope, $sce, Campaigns){
 
-    var self = this;
-
     // Assign the site model to this controller's `site` attr
     this.campaigns = Campaigns;
+
+    var Global = $scope.$parent.glb;
     
     // Use the site model's get method to retrieve the site info
     var promise = this.campaigns.getCampaigns(); // Todo: Include the Id dynamically
@@ -19,16 +19,14 @@ strikestarter.controller('Homepage', [ '$scope', '$sce', 'Campaigns', function($
             console.log(data);
             
             // Bind to the Site obj
-            // angular.extend(this, data.data);
+            angular.extend(this, data);
+
+            this.thing = "rad";
 
             // Bind data to $scope which is then used in directives that need to do stuff when this data arrives
             // $scope.loadedSite = data.data;
 
-            // Convert description (which containts HTML) to parsed string
-            // Implement in view within Global scope using ng-html-bind="descriptionToHtml"
-            // $scope.descriptionToHtml = $sce.trustAsHtml(this.homepage.description);
-
-        }, Campaigns)) // Pass in the Campaigns object for the data to bind to
+        }, Global)) // Pass in the Campaigns object for the data to bind to
         .error( function(data, status, headers, config) {
             var error = 'error';
             error += '/n data: ' + data;
