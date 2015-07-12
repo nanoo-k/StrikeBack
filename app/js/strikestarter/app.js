a9264e6ef89a0ad6019e206e08293555429ad1b1
Miguel Valencia <miguel.s.valencia@gmail.com>
2015-07-12 15:52:13 -0700

    View is not data-ui-route.

--- app/js/strikestarter/app.js
+++ app/js/strikestarter/app.js
@@ -3,26 +3,26 @@
 // Create template-path variable for easy maintenance
 var path = '/web/templates/';
 
-var strikestarter = angular.module('strikestarter', ['ui.router', 'lodash', 'ngSanitize'], [ '$interpolateProvider', function($interpolateProvider) {
+var strikestarter = angular.module('strikestarter', ['ui.router', 'lodash', 'ngSanitize'], [ '$interpolateProvider', function ($interpolateProvider) {
     // Twig uses {{ }}, so Angular now uses {[{ }]}
     $interpolateProvider.startSymbol("{{");
     return $interpolateProvider.endSymbol("}}");
 }]);
 
-strikestarter.config([ '$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+strikestarter.config([ '$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
     
     $locationProvider.html5Mode(true);
 
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
-    .state('home', {
-        url: '/',
-        templateUrl: path + "/homepage.html",
-        controller: "Homepage"
-    })
-    .state('createCampaign', {
-        url: path + 'create_campaign.html',
-        controller: "CreateCampaign"
-    })
+        .state('home', {
+            url: '/',
+            templateUrl: path + "/homepage.html",
+            controller: "Homepage"
+        })
+        .state('createCampaign', {
+            url: path + 'create_campaign.html',
+            controller: "CreateCampaign"
+        });
 }]);
