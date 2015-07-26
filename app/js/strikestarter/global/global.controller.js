d07eb2139ab19f3c987d602c097c1e9e35ca7be8
Miguel Valencia <miguel.s.valencia@gmail.com>
2015-07-26 11:59:36 -0700

    Include the Campaigns collection on the global scope.

--- app/js/strikestarter/global/global.controller.js
+++ app/js/strikestarter/global/global.controller.js
@@ -4,8 +4,9 @@
 
 // var strikestarter = angular.module('strikestarter');
 
-strikestarter.controller('Global', [ '$scope', '$sce', 'User', function ($scope, $sce, User) {
+strikestarter.controller('Global', [ '$scope', '$sce', 'User', 'Campaigns', function ($scope, $sce, User, Campaigns) {
 
     this.user = User;
+    this.campaigns = Campaigns;
 
 }]);
